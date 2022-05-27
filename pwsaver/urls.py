59c69d08cd618cad1c67a84de17ec8d6b2b872e4
ChristC9 <nyanhtetaungucsy106@gmail.com>
2022-05-27 17:29:41 +0630

    forgot pw & pw reset routes

--- pwsaver/urls.py
+++ pwsaver/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import (PasswordResetView,PasswordResetDoneView,
+PasswordResetConfirmView,PasswordResetCompleteView)
 
 
 ## TODO: Add urls for the views
@@ -8,5 +10,10 @@ urlpatterns = [
     path('',createCred,name='createCred'),
     path('get/',credenialList,name='credenialList'),
     path('login/',userlogin,name='user-login'),
-    path('logout/',userlogout,name='user-logout')
+    path('logout/',userlogout,name='user-logout'),
+    path('register/',userregister,name='user-register'),
+    path('pw/reset/',PasswordResetView.as_view(template_name='pwsaver/password_reset.html'),name='password_reset'),
+    path('pw/reset/done/',PasswordResetDoneView.as_view(template_name='pwsaver/password_reset_done.html'),name='password_reset_done'),
+    path('pw/reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='pwsaver/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('pw/reset/complete/',PasswordResetCompleteView.as_view(template_name='pwsaver/password_reset_complete.html'),name='password_reset_complete'),
 ]
