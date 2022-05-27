af85ae2ce40213929d7c126d90cdc05f6196313c
ChristC9 <nyanhtetaungucsy106@gmail.com>
2022-05-27 18:21:54 +0630

    add email credentials to dotenv

--- base/settings.py
+++ base/settings.py
@@ -55,11 +55,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'base.wsgi.application'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = '3b500acae3c41d'
-EMAIL_HOST_PASSWORD = 'd0eefcdb8a6c1b'
-EMAIL_PORT = '2525'
-
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
 DATABASES = {
     'default': {
